953b170f8d63e960f0b397ff50c64af2c5c3b639
Lucas Hideki <ls_hdk@protonmail.com>
2020-12-16 17:18:10 -0300

    dashboard_header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
-import Button from '../../Components/Button';
-import { Title, Container } from './styles';
+import { useNavigation } from '@react-navigation/native';
+import React, { useCallback } from 'react';
 import { useAuth } from '../../hooks/auth';
 
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+} from './styles';
+
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+  const { navigate } = useNavigation();
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile');
+  }, [navigate]);
   return (
     <>
       <Container>
-        <Title>Hello Dashboard</Title>
-        <Button onPress={signOut}>Deslogar</Button>
+        <Header>
+          <HeaderTitle>
+            Bem vindo
+            {'\n'}
+            <UserName>{user.name}</UserName>
+          </HeaderTitle>
+          <ProfileButton onPress={navigateToProfile}>
+            <UserAvatar source={{ uri: user.avatar_url }} />
+          </ProfileButton>
+        </Header>
       </Container>
     </>
   );
